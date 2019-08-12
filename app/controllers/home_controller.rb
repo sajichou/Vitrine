2c695fe07a7e2e490ea90e54a9f883aa5729bfdf
sajid <sajid123@hotmail.fr>
2019-08-12 15:36:22 +0200

    V1-5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
 		p params[:employees]
 		redirect_to controller:"home", action:"accueil"
 		HomeMailer.contacter_email(params[:prenom], params[:nom], params[:email], params[:employees], params[:phone]).deliver_now
-		flash[:notice] = "Merci pour votre intérêt. Nous vous contactons dans les plus brefs délais."
+		flash[:notice] = "Merci pour l'intérêt que vous nous portez. Nous vous contactons dans les plus brefs délais."
 	end
 
 	def demo
